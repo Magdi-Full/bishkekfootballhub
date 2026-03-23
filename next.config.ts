b1eb0a20d1bf41b2ede03f540266ce48c14139ec
Magdi-Full <magdifootball@gmail.com>
2026-03-23 22:36:29 +0600

    jake

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        port: "",
         pathname: "/**",
       },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
